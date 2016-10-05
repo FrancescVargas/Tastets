06232b86a0e08e844adbdcf9ac7339eb8f6a88a8
FrancescVargas <fravase76@gmail.com>
2016-10-05 10:08:04 +0200

    eliminant tastets

---
+++ editartastet.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="es">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width; initial-scale=1.0">
+        <title>Usuari <?php echo $_POST["dni"]; ?></title>
+ <link rel="stylesheet" type="text/css" href="css/estils.css">
+    </head>
+      
+    <body>
+          
+      <header>
+            <H1>FES UN TAST A L'ENGINYERIA</H1>
+            <img id="logo" src="/Francesc/Tastets/vista/imatges/logo2.jpg" alt="logo">
+            <img id="imatgeheader" src="/Francesc/Tastets/vista/imatges/capcalera_recurs_8.jpg">
+            <button><a href="sortirzonaprovada.php">Sortir de la zona Privada</a></button><br>
+            
+            
+        </header>
+        <div id="contenedor">
+            
+            
+            
+            
+            
+        
+            
+            
+            
+        </div>
+    </body>
+</html>
--- zonaprivada.php
+++ zonaprivada.php
@@ -4,89 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width; initial-scale=1.0">
         <title>Usuari <?php echo $_POST["dni"]; ?></title>
-          <style>
-        body
-            {
-                width: 75%;
-                margin: auto;
-            }
-        header
-            {
-                margin-bottom: 30px;
-            }
-        header h1
-            {
-                display: inline-block;
-                width: 70%;
-            }
-        #imatgeheader
-            {
-                width: 100%;
-            }
-        #logo
-            {
-                float: right;
-                margin: 20px 0;
-                width: 30%;
-            }
-        #identificador input,button
-            {
-                font-size: 0.6em;
-                text-align: left;  
-            }
-
-
-        table,th,td
-        {
-        border: 1px solid black;
-            }
-        #video
-            {
-                float: right;
-            }
-        iframe
-            {
-                display: block;
-            }
-        #detalls
-            {
-                border:2px solid blue;
-                border-radius: 50px;
-                margin: 10px;
-                width: 50%;
-                text-align: center;
-            }
-        #fotodesc
-            {
-                float: right;
-            }
-        
-        #forminscripcio
-            {
-                float: right;
-                
-                
-            }
-        #formescondido
-            {
-               
-                box-shadow: 10px 10px 10px blue;
-                margin-top: 20px;
-                border: 1px solid blue;
-                display:none;
-            }
-        #cargarform
-            {
-                float: right;
-                
-                margin: 5px;
-            }
-        input, label, textarea
-            {
-                margin: 0 5px;
-            }
-        
-        </style>
+ <link rel="stylesheet" type="text/css" href="css/estils.css">
     </head>
       
     <body>
@@ -108,6 +26,7 @@ session_start();
  
 if(isset($_POST["dni"]))
     {   
+    
         if($_POST['usuari']!=="" && $_POST['dni']!=="" )
         {
           $_SESSION['usuari']=$_POST['usuari'];
@@ -136,24 +55,37 @@ if(isset($_SESSION['dni']))
         $sel="SELECT * from tastets where tastets.dni='".$_SESSION["dni"]."';";
         $res=$con->query($sel);
         $res=$res->fetchAll();
-        
-        
         echo "<h3>Usuari ".$_SESSION["usuari"]."</h3>";
     
         if(count($res)>0)
         {    
-            
-            
-            echo "<h4>Administració Tastets Propis</4>";
+            echo "<table>
+            <caption><h4>Administració Tastets Propis</4></caption>
+            <tr><th>Nom del Tastet</th><th>Peticions</th><th>Eliminar</th><th>Modificar</th></tr>";
             foreach($res as $fila)
             {
             $sel2="SELECT count(*) from solicituts where solicituts.tastet_id=".$fila["id"].";"; 
             $res2=$con->query($sel2);
             $res2=$res2->fetch();
-            echo $fila["nom"]."-->Numero Peticions -->".$res2[0]."<br>";
+            echo "<tr><td>".$fila["nom"]."</td><td>".$res2[0]."</td><td><a title='eliminar tastet' href='zonaprivada.php?id_borrar=".$fila['id']."'><img class='eliminar' alt='eliminar tastet' src='vista/imatges/eliminar.png'></a></td><td><a title='editar tastet' href='editartastet.php?id=".$fila['id']."' ><img class='eliminar' alt='editar tastet' src=vista/imatges/modificar.png ></a></td></tr>";
             }
+            echo "</table>";
         }
         else echo "<h4>Ara mateix no ets responsable de cap curs</h4>";
+    
+    
+        if(isset ( $_GET['id_borrar']))
+        {
+
+            // borrar el tastet
+
+
+
+         $sql = "delete from tastets where id='".$_GET['id_borrar']."';";
+         $res=$con->exec($sql); 
+         header ("Location:zonaprivada.php");
+
+        } 
         
      }
 ?>
